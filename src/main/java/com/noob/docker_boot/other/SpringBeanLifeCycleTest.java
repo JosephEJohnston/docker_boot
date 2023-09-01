fcd25f0a5fadec740f18b8928455d606e2a3235e
JosephEJohnston <1193347519@qq.com>
2023-09-01 18:46:45 +0800

    生命周期测试

---
+++ src/main/java/com/noob/docker_boot/other/SpringBeanLifeCycleTest.java
@@ -0,0 +1,74 @@
+package com.noob.docker_boot.other;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanLifeCycleTest implements
+        BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean {
+
+    public SpringBeanLifeCycleTest() {
+        System.out.println("我的构造方法执行了。。。。。。。。。。");
+    }
+
+    private String name;
+
+    @Value("张三")
+    public void setName(String name) {
+        System.out.println("setName 方法执行了。。。。。。。。");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("(BeanNameAware)setBeanName 方法执行了。。。。。。。。");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("(BeanFactoryAware)setBeanFactory 方法执行了。。。。。。。。");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("(ApplicationContextAware)setApplicationContext 方法执行了。。。。。。。。");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("(InitializingBean)afterPropertiesSet 方法执行了。。。。。。。。");
+    }
+
+    // 依赖注入完成后自动调用
+    @PostConstruct
+    public void init() {
+        System.out.println("(PostConstruct)init 方法执行了。。。。。。。。");
+    }
+
+    @PreDestroy
+    private void destroy() {
+        System.out.println("(PreDestroy)destroy 方法执行了。。。。。。。。");
+    }
+
+    /*
+        我的构造方法执行了。。。。。。。。。。
+        setName 方法执行了。。。。。。。。
+        (BeanNameAware)setBeanName 方法执行了。。。。。。。。
+        (BeanFactoryAware)setBeanFactory 方法执行了。。。。。。。。
+        (ApplicationContextAware)setApplicationContext 方法执行了。。。。。。。。
+        before 执行了 -> springBeanLifeCycleTest
+        (PostConstruct)init 方法执行了。。。。。。。。
+        (InitializingBean)afterPropertiesSet 方法执行了。。。。。。。。
+        after 执行了 -> springBeanLifeCycleTest
+
+        (PreDestroy)destroy 方法执行了。。。。。。。。
+     */
+}
---
+++ src/main/java/com/noob/docker_boot/other/SpringBeanLifeCycleTestProcessor.java
@@ -0,0 +1,40 @@
+package com.noob.docker_boot.other;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.InvocationHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanLifeCycleTestProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (beanName.equals("springBeanLifeCycleTest")) {
+            System.out.println("before 执行了 -> springBeanLifeCycleTest");
+        }
+
+        return bean;
+    }
+
+    // 可以在这步做代理
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (beanName.equals("springBeanLifeCycleTest")) {
+            System.out.println("after 执行了 -> springBeanLifeCycleTest");
+
+            Enhancer enhancer = new Enhancer();
+
+            enhancer.setSuperclass(bean.getClass());
+            enhancer.setCallback((InvocationHandler) (proxy, method, args) -> {
+                System.out.println("after: 我被增强了");
+                return method.invoke(method, args);
+            });
+
+            return enhancer.create();
+        }
+
+        return bean;
+    }
+}
